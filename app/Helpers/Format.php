36a0371c13fec08f24c4d2227901a55533b13643
Febryan Pratama <febryancaesarpratama@gmail.com>
2025-05-14 16:11:07 +0700

    fix main

--- app/Helpers/Format.php
+++ app/Helpers/Format.php
@@ -24,4 +24,37 @@ class Format
         // Return meta tags as a single string
         return implode("\n", $metaTags);
     }
+        public static function getSubdomain()
+    {
+        try {
+            
+            // curl ssl false
+            $curl = curl_init();
+            curl_setopt_array($curl, [
+                CURLOPT_URL => "https://blog.indonesiacore.com/api/domain/1",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "GET",
+                CURLOPT_SSL_VERIFYPEER => false,
+            ]);
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+            curl_close($curl);
+    
+            if ($err) {
+                return [];
+            }
+    
+            $response = json_decode($response, true);
+    
+            // dd($response);
+    
+            return $response['data'] ?? [];
+        } catch (\Exception $e) {
+            // dd($e);
+            // \Log::error("Error getting subdomain: " . $e->getMessage());
+            return null;
+        }
+    }
 }
